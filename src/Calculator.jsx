1ad8ccde04ca965b9afdc70df165b133ee7dd9ac
Rohini <rohinigunjal115@gmail.com>
2026-02-20 20:41:15 +0530

    xcalculator

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,68 +1,55 @@
-/* eslint-disable no-new-func */
-import React, { useState } from "react";
+import React, {useState} from "react";
 import styles from "./Calculator.module.css";
 
-const Calculator = () => {
+const Calculator = () =>
+{
   const [data, setData] = useState("");
   const [result, setResult] = useState(null);
 
-  function getData(event) {
-    setData(prev => prev + event.target.value);
+  function getData(event){
+      setData(data.concat(event.target.value));
   }
 
-  function clearAll() {
+  function clearAll(){
     setData("");
-    setResult(null);
+    setResult("");
   }
-  function getResult() {
-    if (!data) {
-      setResult("Error");
-      return;
-    }
 
-    try {
-      const safe = data.replace(/[^0-9+\-*/().\s]/g, "");
-      const fn = new Function(`"use strict"; return (${safe})`);
-      const value = fn();
-
-      if (typeof value !== "number" || Number.isNaN(value) || !Number.isFinite(value)) {
-        setResult("Error");
-      } else {
-        setResult(value);
-      }
-    } catch (e) {
+  function getResult(){
+    if(data===""){
       setResult("Error");
     }
+    else{
+       // eslint-disable-next-line no-eval
+       setResult(eval(data));
+    }
+   
   }
 
   return (
-    <div className={styles.calc_wrap}>
+      <div className={styles.calc_wrap} >
       <h1>React Calculator</h1>
-      <input type="text" value={data} readOnly />
+      <input type="text" value={data}/>
       <div className={styles.res}>{result}</div>
       <div className={styles.btn_wrap}>
-        <button onClick={getData} value="7">7</button>
-        <button onClick={getData} value="8">8</button>
-        <button onClick={getData} value="9">9</button>
-        <button onClick={getData} value="+">+</button>
-
-        <button onClick={getData} value="4">4</button>
-        <button onClick={getData} value="5">5</button>
-        <button onClick={getData} value="6">6</button>
-        <button onClick={getData} value="-">-</button>
-
-        <button onClick={getData} value="1">1</button>
-        <button onClick={getData} value="2">2</button>
-        <button onClick={getData} value="3">3</button>
-        <button onClick={getData} value="*">*</button>
-
-        <button onClick={clearAll}>C</button>
-        <button onClick={getData} value="0">0</button>
-        <button onClick={getResult}>=</button>
-        <button onClick={getData} value="/">/</button>
+      <button onClick={getData} value="7">7</button>
+      <button onClick={getData} value="8">8</button>
+      <button onClick={getData} value="9">9</button>
+      <button onClick={getData} value="+">+</button>            
+      <button onClick={getData} value="4">4</button>
+      <button onClick={getData} value="5">5</button>
+      <button onClick={getData} value="6">6</button>
+      <button onClick={getData} value="-">-</button>            
+      <button onClick={getData} value="1">1</button>
+      <button onClick={getData} value="2">2</button>      
+      <button onClick={getData} value="3">3</button>
+      <button onClick={getData} value="*">*</button>            
+      <button onClick={clearAll}>C</button>
+      <button onClick={getData} value="0">0</button>
+      <button onClick={getResult}>=</button>
+      <button onClick={getData} value="/">/</button>
       </div>
-    </div>
-  );
-};
+    </div>   
+  );}
 
 export default Calculator;
